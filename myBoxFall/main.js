ccc375c33855ea44758b4403821dfbc8ef09d8f3
Gus700 <gustavocruz700@gmail.com>
2022-10-13 12:10:17 -0700

    testing the game files to run on the github pages

--- myBoxFall/main.js
+++ myBoxFall/main.js
@@ -1,154 +1,181 @@
-title = "Box Fall";
+title = "SpaceJump";
 
 description = `
-Avoid the boxes
 `;
 
 characters = [
 `
- l l l 
-lllllll
-ll l ll
-ll l ll
-lllllll
-ll   ll
-ll   ll
-`,`
-lllll
-lllll
-lllll
+ cccc 
+cccccc
+ cccc
+cccccc
+  cc
+cc  cc
 `
 ];
-
 const G = {
-	WIDTH: 100,
-	HEIGHT: 150,
+	WIDTH: 150,
+	HEIGHT: 100,
+	STAR_SPEED_MIN: 0.5,
+	STAR_SPEED_MAX: 1.0,
+	MAX_JUMP_HEIGHT: 35
+};
 
-    PLATFORM_X: 0,
-    PLATFORM_Y: 140,
-    PLATFORM_SIZE_X: 100,
-    PLATFORM_SIZE_Y: 10,
-
-	BOX_SPEED: 1.0,
-    MAX_BOXES: 12,
-
-    PLAYER_SPEED: 1
-}
-
-const COLORS = ["red", "blue", "yellow", "purple"];
-
-// JSDocs
-/**
- * @typedef {{
- * pos: Vector,
- * speed: number,
- * clr: string
- * }} Box
- */
+options = { 
+	viewSize: {x:G.WIDTH, y:G.HEIGHT},
+	theme: "dark",
+	isReplayEnabled: true
+};
 
 /**
- * @type { Box [] }
- */
-let boxes;
+* @typedef {{
+* pos: Vector,
+* speed: number
+* }} Star
+*/
+
+/**
+* @type { Star [] }
+*/
+let stars;
 
 /**
  * @typedef {{
  * pos: Vector,
  * speed: number,
- * direction: number
+ * sizeX: number,
+ * sizeY: number
+ * }} Obst
+ */
+
+/**
+ * @type { Obst [] }
+ */
+let obst = [];
+
+/**
+ * @type { Obst [] }
+ */
+ let topObst = [];
+
+/**
+ * @typedef {{
+ * pos: Vector,
  * }} Player
  */
 
-/**
+/** 
  * @type { Player }
- */
+*/
 let player;
 
-
-options = {
-	viewSize: {x: G.WIDTH, y: G.HEIGHT},
-    isCapturing: true,
-    isCapturingGameCanvasOnly: true,
-    captureCanvasScale: 2,
-    seed: 1,
-    isPlayingBgm: true,
-    isReplayEnabled: true,
-    theme: "dark"
-};
-
+let jumping = false;
+let falling = false;
+let ObstSpawnX = G.WIDTH;
+let ObstSpawnY = G.HEIGHT * 0.9;
+let ObstSpeed = 1;
 function update() {
+	// The init function running at startup
 	if (!ticks) {
-        player = {
-            pos: vec(G.WIDTH/2, G.PLATFORM_Y - 2),
-            speed: G.PLAYER_SPEED,
-            direction: 1
-        }
+		//initialize the stars
+		stars = times(20, () => {
+			const posX = rnd(0, G.WIDTH);
+			const posY = rnd(0, G.HEIGHT * 0.7);
+			return {
+				pos: vec(posX, posY),
+				speed: rnd(G.STAR_SPEED_MIN, G.STAR_SPEED_MAX)
+			};
+		});
+		// initialize the obstacles
+		topObst = [];
+		obst = times(3 , () => {
+			const posX = ObstSpawnX += 100;
+			const posY = rnd(ObstSpawnY, ObstSpawnY - 15)
+			// initialize the complimentary top Obstacles
+			topObst.push({
+				pos: vec(posX, posY - 60),
+				speed: ObstSpeed,
+				sizeX: 5, 
+				sizeY: 60
+			});
+			return {
+				pos: vec(posX, posY),
+				speed: ObstSpeed,
+				sizeX: 5, 
+				sizeY: 20
+			};
 
-        boxes = [];
+		});
+		//initialize the player
+		player = {
+			pos: vec(G.WIDTH * 0.4, G.HEIGHT * 0.7)
+		};
 	}
-
-    // Create Platform
-    color("cyan");
-    rect(G.PLATFORM_X, G.PLATFORM_Y, G.PLATFORM_SIZE_X, G.PLATFORM_SIZE_Y);
-
-    // Draw Player
-    color("red");
+	// update for the stars that were created above
+	stars.forEach((s) => {
+		s.pos.x -= s.speed;
+		s.pos.wrap(0, G.WIDTH, 0, G.HEIGHT);
+		color("yellow");
+		box(s.pos, 1);
+	});
+	// draw obstacles
+	obst.forEach((o) => {
+		o.pos.x -= o.speed;
+		o.pos.wrap(0, G.WIDTH, 0, G.HEIGHT);
+		color("blue");
+		box(o.pos, o.sizeX, o.sizeY);
+	});
+	//draw top Obstacles
+	topObst.forEach((o) => {
+		o.pos.x -= o.speed;
+		o.pos.wrap(0, G.WIDTH, 0, G.HEIGHT);
+		color("blue");
+		box(o.pos, o.sizeX, o.sizeY);
+	});
+	// draw floor
+	color("blue");
+	box (G.WIDTH * 0.5, G.HEIGHT * 0.9, G.WIDTH, 20);
+	// draw the player
+	color("cyan");
     char("a", player.pos);
+	// jump when pressing the button
+	if (input.isJustPressed) {
+		jumping = true;
+	}
+	// hold to jump higher
+	if (input.isPressed) {
+		if (player.pos.y > (G.HEIGHT * 0.7 - G.MAX_JUMP_HEIGHT) && jumping == true && falling != true) {
+			player.pos.y -= 3;
+		} else { // if reached max jumping height then signal jump is over
+		jumping = false;
+		}
+	}
+	// if button is release early, signal jump over
+	if (input.isJustReleased){
+		jumping = false;
+	}
+	// makes sure the player is brought down
+	if (!(char("a", player.pos).isColliding.rect.blue) && jumping == false ||
+	((char("a", player.pos).isColliding.rect.blue) && player.pos.y < G.HEIGHT * 0.6)) {
+		falling = true;
+		player.pos.y += 2;
+	} else {
+		falling = false;
+	}
+	// collisions with obstacle or floor will move the character back
+	if (char("a", player.pos).isColliding.rect.blue) {
+		//console.log("Collided with an obstacle")
+		player.pos.x -= ObstSpeed;
+	// keep the character moving foward if they are not on the ground or touching an obstacle
+	} else if (!(char("a", player.pos).isColliding.rect.blue) && player.pos.x < G.WIDTH * 0.6) {
+		player.pos.x += ObstSpeed / 4;
+	}
+	// if the player has reached the end of the screen then make it end
+	if( player.pos.x <= 0) {
+		topObst = [];
+		end();
+	}
+	addScore(1);
 
-    // Spawn Boxes
-    if (boxes.length < G.MAX_BOXES) {
-        // const posX1 = rnd(5, G.WIDTH/2)
-        // const posX2 = rnd(G.WIDTH/2, G.WIDTH - 5);
-        // const posY = rnd(75, 100);
-
-        // boxes.push({pos: vec(posX1, posY) , speed: G.BOX_SPEED});
-        // boxes.push({pos: vec(posX2, posY) , speed: G.BOX_SPEED});   
-
-        for (let i=0; i < G.MAX_BOXES - boxes.length; i++) {
-            const posX = rnd(0, G.WIDTH);
-            const posY = rnd(0, -150);
-            boxes.push({
-                pos: vec(posX, posY),
-                speed: G.BOX_SPEED,
-                clr: COLORS[Math.floor(Math.random()*COLORS.length)]
-            });
-        }
-    }
-
-    if (input.isJustPressed || (player.pos.x < 0 && player.direction < 0) || (player.pos.x > G.WIDTH && player.direction > 0) ) {
-        player.direction *= -1;
-    }
-    player.pos.x += player.direction * sqrt(difficulty);
-
-
-    // Remove Boxes
-    remove(boxes, (b) => {
-        b.pos.y += G.BOX_SPEED;
-        // color("yellow");
-        // let rnd_color = COLORS[Math.floor(Math.random()*COLORS.length)];
-        // color(rnd_color);
-        // char("b", b.pos);
-        // box(b.pos, 2);
-
-        color(b.clr);
-        const isCollidingWithPlatform = char("b", b.pos).isColliding.rect.cyan;
-        if (isCollidingWithPlatform) {
-            // let rnd_color = COLORS[Math.floor(Math.random()*COLORS.length)];
-            // color(rnd_color);
-            // color("red");
-            play("explosion");
-            particle(b.pos);
-            addScore(1);
-        }
-
-        const isCollidingWithPlayer = char("b", b.pos).isColliding.char.a;
-
-        if (isCollidingWithPlayer) {
-            end();
-            play("powerUp");
-        }
-
-        return (b.pos.y > G.PLATFORM_Y);
-    });
-
+	player.pos.clamp(0, G.WIDTH, 0, G.HEIGHT);
 }
